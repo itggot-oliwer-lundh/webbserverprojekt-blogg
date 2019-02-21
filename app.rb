30cd68793fce08b63eacae95f92f331b5c5fde0e
itggot-oliwer-lundh <38528588+itggot-oliwer-lundh@users.noreply.github.com>
2019-02-21 10:51:24 +0100

    la till login unktion + en profil sida

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ get('/sign_up') do
 end 
 
 post('/sign_up') do
-    db = SQLite3::Database.new("db/users.db")
+    db = SQLite3::Database.new("db/bloggdatabas.db")
     db.results_as_hash = true
 
     existing_user = db.execute("SELECT username FROM user_login WHERE username=?", params["username"])
@@ -26,5 +26,35 @@ post('/sign_up') do
 
     db.execute("INSERT INTO user_login(username, password) VALUES (?, ?)", [params["username"], password])
 
-    redirect('/users')
+    redirect('/')
+end
+
+get('/login') do
+    slim(:login)
+end 
+
+post('/login') do
+    db = SQLite3::Database.new("db/bloggdatabas.db")
+    db.results_as_hash = true
+    
+    existing_user = db.execute("SELECT password FROM user_login WHERE username=?", [params["username"]])
+
+    if existing_user.length == 0
+        redirect(:login)
+    end
+
+    password_hash = existing_user[0]["password"]
+
+    password_match = BCrypt::Password.new(password_hash) == params["password"]
+
+    if password_match
+        session[:username] = params["username"]
+        redirect("/profile/#{params["username"]}")
+    else
+        redirect(:login)
+    end
+end
+
+get('/profile/:username') do
+    slim(:profile, locals:{user:params["username"]})
 end
